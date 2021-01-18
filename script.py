67a591a768d12280c03f985ca578b3b808c311cb
aa-ag <aaron.aguerrevere@gmail.com>
2021-01-18 16:08:07 -0600

    Popen class

--- script.py
+++ script.py
@@ -1,4 +1,14 @@
 import subprocess
 
-# Access contents of subprocess module
-print(dir(subprocess))
+# define command, as string
+command = 'ls -ltr'
+
+# execute command using shell
+# Popen class: underlyng process creation
+sp = subprocess.Popen(command, shell=True)
+
+# store return code in variable
+rc = sp.wait()
+
+# print contents
+print(sp)
