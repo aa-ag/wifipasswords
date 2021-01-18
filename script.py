88521dd081bacf18d068ac75510efea97603427e
aa-ag <aaron.aguerrevere@gmail.com>
2021-01-18 16:26:12 -0600

    added settings.py (ignored) + terminal command that works

--- script.py
+++ script.py
@@ -1,14 +1,3 @@
-import subprocess
-
-cmd = 'ping -c 5 google.com'
-sp = subprocess.Popen(cmd,
-                      shell=True,
-                      stdout=subprocess.PIPE,
-                      stderr=subprocess.PIPE)
-rc = sp.wait()
-
-out, err = sp.communicate()
-
-print('rc:', rc, '\n')
-print('output: \n', out)
-print('error: \n', err)
+'''
+security find-generic-password - ga WIFI NAME | grep “password: ”
+'''
